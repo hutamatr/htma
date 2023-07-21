bf57bc052c9e6ba82d773138dcb7900045f220eb
hutamatr <hutamatr@gmail.com>
2023-07-21 17:16:30 +0700

    feat: create top navigation page

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -10,21 +10,15 @@ export default function Navigation() {
   const menuShowHandler = () => setIsMenuShow((prevState) => !prevState);
 
   return (
-    <header className='fixed top-0 w-full z-[1090] flex-wrap flex items-center nav-height'>
-      <nav className='container-width'>
-        <div className='grid-12'>
-          <div className='col-start-1 col-end-1'>
-            <h1
-              className={clsx(
-                'rounded-sm bg-custom-black',
-                'text-xl font-semibold text-custom-green',
-                'py-1 px-2'
-              )}
-            >
+    <header className='nav-height fixed top-0 z-[1090] flex w-full flex-wrap items-center'>
+      <nav className='layout'>
+        <div className={clsx('grid-12 gap-2', 'md:gap-6')}>
+          <div className='col-start-1'>
+            <h1 className='w-fit rounded-sm bg-custom-black px-2 py-1 text-xl font-semibold text-custom-green'>
               HTMA.
             </h1>
           </div>
-          <div className='col-start-12 col-end-12 block mx-auto'>
+          <div className='col-start-12 col-end-12 mx-auto block'>
             <button onClick={menuShowHandler} className='z-[60] mt-1'>
               <MdGraphicEq
                 className={clsx(
