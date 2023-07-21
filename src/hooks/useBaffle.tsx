5ba7e0d27aa261165b8086978223f9806efbec95
hutamatr <hutamatr@gmail.com>
2023-07-21 17:14:43 +0700

    feat: add font baffle custom hook

---
+++ src/hooks/useBaffle.tsx
@@ -0,0 +1,18 @@
+import baffle from 'baffle';
+
+const useBaffle = (classNameSelector: string) => {
+  const newBaffle = () => {
+    const target = baffle(classNameSelector);
+    target.set({
+      characters: '█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█<░▒ ▓/░>',
+      speed: 100,
+    });
+    target.start();
+    target.reveal(1000, 100);
+    target.stop();
+  };
+
+  return { newBaffle };
+};
+
+export default useBaffle;
