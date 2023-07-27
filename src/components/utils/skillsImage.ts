d801abf8ab46e993f444803b26342f44d8e0ed0a
hutamatr <hutamatr@gmail.com>
2023-07-27 23:17:57 +0700

    feat: add skills image data & dummy portfolio data

---
+++ src/components/utils/skillsImage.ts
@@ -0,0 +1,54 @@
+import CSS from '@public/assets/images/skills/css-3.webp';
+import HTML from '@public/assets/images/skills/html-5.webp';
+import JS from '@public/assets/images/skills/js.webp';
+import NEXT_JS from '@public/assets/images/skills/nextjs.webp';
+import REACT from '@public/assets/images/skills/react.webp';
+import REACT_NATIVE from '@public/assets/images/skills/react-native.webp';
+import TAILWIND from '@public/assets/images/skills/tailwind.webp';
+import TS from '@public/assets/images/skills/typescript.webp';
+
+export const langSkillsImage = [
+  {
+    id: '01',
+    image: HTML.src,
+    title: 'HTML',
+  },
+  {
+    id: '02',
+    image: CSS.src,
+    title: 'CSS',
+  },
+  {
+    id: '03',
+    image: JS.src,
+    title: 'JavaScript',
+  },
+  {
+    id: '04',
+    image: TS.src,
+    title: 'TypeScript',
+  },
+];
+
+export const libFrameSkillsImage = [
+  {
+    id: '05',
+    image: REACT.src,
+    title: 'React',
+  },
+  {
+    id: '06',
+    image: REACT_NATIVE.src,
+    title: 'React Native',
+  },
+  {
+    id: '07',
+    image: NEXT_JS.src,
+    title: 'Next.js',
+  },
+  {
+    id: '08',
+    image: TAILWIND.src,
+    title: 'Tailwind',
+  },
+];
