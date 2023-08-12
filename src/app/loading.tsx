309a007f25d6f4d42746d77d884a6d5b13899308
hutamatr <hutamatr@gmail.com>
2023-08-12 22:10:40 +0700

    style: add style on loading spin

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,11 @@
-import LoadingSpin from '@components/ui/LoadingSpin';
+import clsx from 'clsx';
+
+import LoadingSpin from '@components/ui/loading-spin';
 
 export default function Loading() {
   return (
     <div className='col-start-7 flex h-screen w-full items-center justify-center lg:col-start-8'>
-      <LoadingSpin />
+      <LoadingSpin className={clsx('h-8 w-8', 'md:h-12 md:w-12')} />
     </div>
   );
 }
