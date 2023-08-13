188aa1404c56d4d93e2c9d42f5b8b7c6e4ace0b2
hutamatr <hutamatr@gmail.com>
2023-08-14 02:09:54 +0700

    feat: crate page components wrapper

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,8 @@
-import clsx from 'clsx';
 import { Metadata } from 'next';
 import { Toaster } from 'react-hot-toast';
 
 import Footer from '@components/footer/footer-section';
+import PageWrapper from '@components/ui/page-wrapper';
 
 import Contact from '@modules/contact-page/contact';
 
@@ -15,20 +15,15 @@ export default function ContactPage() {
   return (
     <>
       <Toaster position='top-center' />
-      <section
-        className={clsx(
-          'z-[1000] col-start-3 col-end-12 h-full overflow-y-visible px-4',
-          'md:px-0',
-          'lg:col-start-5'
-        )}
-      >
+
+      <PageWrapper>
         <div className='mb-8 flex flex-wrap pt-[clamp(5.875rem,_0.0294rem_+_7.7941vw,_12.5rem)]'>
           <div className='w-full'>
             <Contact />
             <Footer />
           </div>
         </div>
-      </section>
+      </PageWrapper>
     </>
   );
 }
