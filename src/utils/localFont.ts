33843345310ec93b9c932ae07e5b5405fda96ac1
hutamatr <hutamatr@gmail.com>
2023-08-22 18:04:44 +0700

    style: convert font from ttf to woff2

--- src/utils/localFont.ts
+++ src/utils/localFont.ts
@@ -3,22 +3,22 @@ import localFont from 'next/font/local';
 export const kataGrotesk = localFont({
   src: [
     {
-      path: '../../public/fonts/KataGrotesk-Thin.ttf',
+      path: '../../public/fonts/KataGrotesk-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/KataGrotesk-Regular.ttf',
+      path: '../../public/fonts/KataGrotesk-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/KataGrotesk-SemiBold.ttf',
+      path: '../../public/fonts/KataGrotesk-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/KataGrotesk-Bold.ttf',
+      path: '../../public/fonts/KataGrotesk-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
